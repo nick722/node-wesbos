56e7c151dd4305e9322efbd2a46402cf07cd997b
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-02-04 21:48:08 +0300

    9. Creating our Store Model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = "Nick";
-  if (req.name === "Nick") {
-    throw Error("That is a stupid name");
-  }
-  res.cookie("name", "Nick is awesome", { maxAge: 9000000 });
-  next();
-};
 exports.homePage = (req, res) => {
   console.log("req.name>>>", req.name);
   res.render("index");
